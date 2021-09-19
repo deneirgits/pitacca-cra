772cfcf805a3214edaa3e28783e429a8813d4949
Deneir Uy <deneiruy@gmail.com>
2021-09-19 19:18:33 +0800

    Convert some UI div elements to button

--- src/Record.tsx
+++ src/Record.tsx
@@ -21,30 +21,32 @@ function Record(props: Props) {
   }).format(props.value);
 
   return (
-    <div className="flex py-1.5 text-sm h-17">
-      <div className="pr-3 text-lg">
-        <div className={`${props.categoryColor} rounded-full p-3 text-white`}>
-          <span>{props.categoryIcon}</span>
+    <button className="w-full">
+      <span className="flex py-1.5 text-sm h-17">
+        <div className="pr-3 text-lg">
+          <div className={`${props.categoryColor} rounded-full p-3 text-white`}>
+            <span>{props.categoryIcon}</span>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col flex-grow">
-        <div className="font-medium">{props.categoryName}</div>
-        <div>{props.account}</div>
-        <div className="font-italic text-xs">{`${
-          props.description ? `"${props.description}"` : " "
-        }`}</div>
-      </div>
-      <div className="flex flex-col text-right">
-        <div
-          className={`font-medium ${
-            Math.sign(props.value) === 1 ? "text-rec-inc" : "text-rec-dec"
-          }`}
-        >
-          {props.currency} {localeValue}
+        <div className="flex text-left flex-col flex-grow">
+          <div className="font-medium">{props.categoryName}</div>
+          <div>{props.account}</div>
+          <div className="font-italic text-xs">{`${
+            props.description ? `"${props.description}"` : " "
+          }`}</div>
         </div>
-        <div className="text-xs">{dateFormat.format(props.date)}</div>
-      </div>
-    </div>
+        <div className="flex flex-col text-right">
+          <div
+            className={`font-medium ${
+              Math.sign(props.value) === 1 ? "text-rec-inc" : "text-rec-dec"
+            }`}
+          >
+            {props.currency} {localeValue}
+          </div>
+          <div className="text-xs">{dateFormat.format(props.date)}</div>
+        </div>
+      </span>
+    </button>
   );
 }
 
--- src/RecordList.tsx
+++ src/RecordList.tsx
@@ -11,17 +11,107 @@ function RecordList() {
     <div className="bg-primbg px-2">
       <div className="flex py-1.5">
         <div className="flex-grow font-medium">Records</div>
-        <div className="flex bg-secondary text-sectext rounded-md py-0.5 px-9 shadow">
+        <button className="flex bg-secondary text-sectext rounded-md py-0.5 px-9 shadow">
           <div className="pr-1">
             <span className="text-xl">
               <MdList />
             </span>
           </div>
           <div className="text-sm">All</div>
-        </div>
+        </button>
       </div>
       {/* TODO: Remove placeholder records */}
       <div className="h-96 pb-20 overflow-y-auto">
+        <Record
+          categoryIcon={<MdRestaurant />}
+          categoryColor="bg-acc-amber"
+          categoryName="Restaurant"
+          account="Cash"
+          currency="PHP"
+          date={new Date(2021, 9, 14)}
+          description="Joed's Lutong Hapon"
+          value={-500.0}
+        />
+        <Record
+          categoryIcon={<MdMonetizationOn />}
+          categoryColor="bg-acc-green"
+          categoryName="Income"
+          account="Bank"
+          currency="PHP"
+          date={new Date(2021, 9, 15)}
+          description=""
+          value={2000.0}
+        />
+        <Record
+          categoryIcon={<MdLocalMall />}
+          categoryColor="bg-acc-light-blue"
+          categoryName="Shopping"
+          account="Cash"
+          currency="PHP"
+          date={new Date(2021, 7, 4)}
+          description="Sandal"
+          value={-800.0}
+        />
+        <Record
+          categoryIcon={<MdRestaurant />}
+          categoryColor="bg-acc-amber"
+          categoryName="Restaurant"
+          account="Cash"
+          currency="PHP"
+          date={new Date(2021, 9, 14)}
+          description="Joed's Lutong Hapon"
+          value={-500.0}
+        />
+        <Record
+          categoryIcon={<MdMonetizationOn />}
+          categoryColor="bg-acc-green"
+          categoryName="Income"
+          account="Bank"
+          currency="PHP"
+          date={new Date(2021, 9, 15)}
+          description=""
+          value={2000.0}
+        />
+        <Record
+          categoryIcon={<MdLocalMall />}
+          categoryColor="bg-acc-light-blue"
+          categoryName="Shopping"
+          account="Cash"
+          currency="PHP"
+          date={new Date(2021, 7, 4)}
+          description="Sandal"
+          value={-800.0}
+        />
+        <Record
+          categoryIcon={<MdRestaurant />}
+          categoryColor="bg-acc-amber"
+          categoryName="Restaurant"
+          account="Cash"
+          currency="PHP"
+          date={new Date(2021, 9, 14)}
+          description="Joed's Lutong Hapon"
+          value={-500.0}
+        />
+        <Record
+          categoryIcon={<MdMonetizationOn />}
+          categoryColor="bg-acc-green"
+          categoryName="Income"
+          account="Bank"
+          currency="PHP"
+          date={new Date(2021, 9, 15)}
+          description=""
+          value={2000.0}
+        />
+        <Record
+          categoryIcon={<MdLocalMall />}
+          categoryColor="bg-acc-light-blue"
+          categoryName="Shopping"
+          account="Cash"
+          currency="PHP"
+          date={new Date(2021, 7, 4)}
+          description="Sandal"
+          value={-800.0}
+        />
         <Record
           categoryIcon={<MdRestaurant />}
           categoryColor="bg-acc-amber"
